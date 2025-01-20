ff42247e3712cca08cc786e0968b8f022c378198
gbheemesh888@gmail.com <bgadige@snaplogic.com>
2025-01-20 19:41:22 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+  const[count,setCount] = useState(0);
+
+  const countClicks = ()=>{
+    setCount(click=>click+1);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={countClicks} >Click me</button>
     </div>
   )
 }
